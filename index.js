606fe22d4fb6421de43c9457132c03d6617d425a
f2reninj5 <16m.nowak@myton.co.uk>
2022-03-21 16:35:33 +0000

    Copied template from f2reninj5.xyz

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const { port } = require('./config.json')
+
+app.use(express.static('public'))
+app.use((request, response, next) => {
+
+	response.locals.partialsDir = path.resolve(__dirname, './views/partials')
+	next()
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (request, response) => {
+
+	response.render('home/main.ejs')
+})
+
+let server = app.listen(port, () => {
+
+	console.log(`Listening at http://localhost/.`)
+})
