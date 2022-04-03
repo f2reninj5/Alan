4aa9db1de4b3674339420debddfe204e1de167a3
f2reninj5 <16m.nowak@myton.co.uk>
2022-04-03 16:05:40 +0100

    Implemented functional authorisation feature

--- interface/index.js
+++ interface/index.js
@@ -2,13 +2,18 @@
 const path = require('path')
 const fetch = require('node-fetch')
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 const { Client } = require('../app')
 const { port } = require('./config.json') // PATH: config
 
+app.enable('trust proxy')
+
 app.set('views', path.resolve(__dirname, './views')) // PATH: views
 app.set('view engine', 'ejs')
+
+app.use(cookieParser())
 
 app.use(express.static(path.resolve(__dirname, './public'))) // PATH: public
 
@@ -19,54 +24,70 @@ app.use((request, response, next) => {
 	request.fullHost = request.protocol + '://' + request.headers.host
 	request.fullURL = request.fullHost + request.originalUrl
 
-	let uri = encodeURIComponent(request.fullHost + '/authorise')
-	let scope = 'guilds identify'
-	request.authURI = `https://discord.com/api/oauth2/authorize?client_id=${Client.user.id}&redirect_uri=${uri}&response_type=code&scope=${encodeURIComponent(scope)}`
+	request.authURI = request.fullHost + '/authorise'
+	request.authScope = 'identify guilds'
+	request.authURL = `https://discord.com/api/oauth2/authorize?client_id=${Client.user.id}&redirect_uri=${encodeURIComponent(request.authURI)}&response_type=code&scope=${encodeURIComponent(request.authScope)}`
 	next()
 })
 
-app.get('/', (request, response) => {
+app.get('/', async (request, response) => {
 
-	console.log(Client)
+	if (!await request.cookies.access_token) {
 
-	response.render('home/main.ejs')
+		return response.render('home/main.ejs')
+	}
+
+	let user = await (await fetch('https://discord.com/api/users/@me', {
+
+		headers: {
+
+			authorization: `Bearer ${request.cookies.access_token}`
+		}
+	})).json()
+
+	response.render('home/main-authorised.ejs', {
+		
+		user
+	})
 })
 
 app.get('/authorise', async (request, response) => {
+
+	if (request.cookies.access_token) {
+
+		return response.redirect('/')
+	}
 
 	let code = request.query.code
 
 	if (!code) {
 
-		response.redirect(request.authURI)
-
-		return
+		return response.redirect(request.authURL)
 	}
 
 	// post request
 
-	let tokenResponse = await fetch('https://discord.com/api/oauth2/token', {
+	let authData = await (await fetch('https://discord.com/api/oauth2/token', {
+
 		method: 'POST',
 		body: new URLSearchParams({
+
 			client_id: Client.user.id,
-			client_secret: 'OTU1MTE2NzAyNDE5NTQyMDM2.Yjc_vw.NxAxQyq7AaPOXz8olp97krvUWQA',
+			client_secret: require('../tokens.json').secret,
 			code,
 			grant_type: 'authorization_code',
-			redirect_uri: `http://localhost`,
-			scope: 'identify guilds',
+			redirect_uri: request.authURI,
+			scope: request.authScope,
 		}),
 		headers: {
+
 			'Content-Type': 'application/x-www-form-urlencoded',
 		},
-	})
+	})).json()
 
-	console.log(tokenResponse)
+	response.cookie('access_token', authData.access_token, { expires: new Date(Date.now() + authData.expires_in) })
 
-	// save access token as cookie
-
-	// redirect to dashboard
-
-	response.send('<link href="/styles.css" rel="stylesheet" type="text/css">' + request.query.code)
+	response.redirect('/')
 })
 
 module.exports = app.listen(port, () => {
